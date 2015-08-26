659cd2350973712da488d0f5c3aa1b3eb074e28f
NiuHongbo1989 <906757595@qq.com>
2015-08-26 23:44:38 +0800

    牛洪波 --微信基类

--- src/com/craftsmanasia/wechat/port/CoreServlet.java
+++ src/com/craftsmanasia/wechat/port/CoreServlet.java
@@ -18,10 +18,10 @@ import com.craftsmanasia.wechat.util.SignUtil;
  */ 
 public class CoreServlet extends HttpServlet {  
     private static final long serialVersionUID = 4440739483644821986L;  
-
-   /* *//** 
+  
+    /** 
      * 确认请求来自微信服务器 
-     *//*  
+     */  
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {  
         // 微信加密签名  
         String signature = request.getParameter("signature");  
@@ -41,9 +41,9 @@ public class CoreServlet extends HttpServlet {
         out = null;  
     }  
   
-    *//** 
+    /** 
      * 处理微信服务器发来的消息 
-     *//*  
+     */  
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {  
     	// 将请求、响应的编码均设置为UTF-8（防止中文乱码）  
         request.setCharacterEncoding("UTF-8");  
@@ -57,5 +57,5 @@ public class CoreServlet extends HttpServlet {
         out.print(respMessage);  
         out.close();  
     }  
-  */
+  
 }  
